38ad43eb923e81b24402763d073c76a5b955ab1d
unknown <ahmedalnafy@gmail.com>
2024-09-30 00:51:06 +0300

    last update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { BlankComponent } from './layouts/blank/blank.component';
 import { AuthComponent } from './layouts/auth/auth.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 export const routes: Routes = [
-    {path: "", component:BlankComponent},
+    {path: "", component:BlankComponent, children: [
+        {path:"", component:LandingComponent}
+    ]},
     {path: "", component:AuthComponent, children: [
        {path: "signup", component:SignUpComponent,title:"sign up"},
        {path: "login", component:LoginComponent, title: "login"}
--- src/app/layouts/blank/blank.component.ts
+++ src/app/layouts/blank/blank.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-blank',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent,RouterOutlet],
   templateUrl: './blank.component.html',
   styleUrl: './blank.component.scss'
 })
